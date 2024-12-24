face01fa9fe5035875e62eb48244781ced55ca66
LoliYori <148603196+LoliYori@users.noreply.github.com>
2024-12-25 01:33:42 +0500

    Create FractionInterface.java

---
+++ FractionInterface.java
@@ -0,0 +1,10 @@
+public interface FractionInterface {
+    // Метод для получения вещественного значения дроби
+    double toDouble();
+
+    // Метод для изменения числителя дроби
+    void setNumerator(int numerator);
+
+    // Метод для изменения знаменателя дроби
+    void setDenominator(int denominator);
+}
