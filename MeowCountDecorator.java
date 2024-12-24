8f235b04e4bb84b300755ff595b32b358356e0b0
LoliYori <148603196+LoliYori@users.noreply.github.com>
2024-12-25 01:34:30 +0500

    Create MeowCountDecorator.java

---
+++ MeowCountDecorator.java
@@ -0,0 +1,23 @@
+public class MeowCountDecorator implements Meowable {
+    // Исходный объект, который декорируется
+    private Meowable cat;
+    // Счетчик количества вызовов метода meow
+    private int meowCount = 0;
+
+    // Конструктор принимает объект для декорирования
+    public MeowCountDecorator(Meowable cat) {
+        this.cat = cat;
+    }
+
+    // Переопределение метода meow
+    @Override
+    public void meow() {
+        cat.meow(); // Вызываем метод meow у декорируемого объекта
+        meowCount++; // Увеличиваем счетчик
+    }
+
+    // Метод для получения количества вызовов метода meow
+    public int getMeowCount() {
+        return meowCount;
+    }
+}
