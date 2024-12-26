26a5ae3a0f09f2836133540cd1e5ad4945a1b15c
LoliYori <148603196+LoliYori@users.noreply.github.com>
2024-12-26 09:58:33 +0500

    Update Funs.java

--- Funs.java
+++ Funs.java
@@ -4,7 +4,7 @@ public class Funs {
     // Метод вызывает meow() для каждого объекта в списке
     public static void meowsCare(List<Meowable> cats) {
         for (Meowable cat : cats) {
-            cat.meow(); // Вызываем метод meow у каждого кота
+            cat.meow();
         }
     }
 }
